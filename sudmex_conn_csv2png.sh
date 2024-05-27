ae673780858549b60cf58df600c4d9f2b9d3360a
Luis Concha <lconcha@gmail.com>
2024-05-27 09:47:35 -0600

    nicer output to cli

--- sudmex_conn_csv2png.sh
+++ sudmex_conn_csv2png.sh
@@ -10,7 +10,7 @@ n=$(wc -l $csv | awk '{print $1}')
 
 maxvalue=$(sed 's/,/\n/g' $csv | sed 's/\.[0-9]*//g' | uniq | sort -n | tail -n 1)
 meanvalue=$(sed 's/,/\n/g' $csv | uniq | sort -n | awk '{a+=$1} END{print a/NR}' | sed 's/\.[0-9]*//' )
-echo meanvalue is $meanvalue
+echo "  mean value: $meanvalue ($csv)"
 
 echo P2 > $pgm
 echo "$n $n" >> $pgm
