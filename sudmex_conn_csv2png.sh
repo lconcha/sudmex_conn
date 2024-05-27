1a5b686b4cec0ef7ec2fb47a85ce20a9eb056b20
Luis Concha <lconcha@gmail.com>
2024-05-27 09:40:38 -0600

    improved handling of decimals

--- sudmex_conn_csv2png.sh
+++ sudmex_conn_csv2png.sh
@@ -6,14 +6,15 @@ png=$2
 
 pgm=${png%.png}.pgm
 geom="600x600"
+n=$(wc -l $csv | awk '{print $1}')
 
-maxvalue=$(sed 's/,/\n/g' $csv | sed 's/\.[0-9]*//' | uniq | sort -n | tail -n 1)
+maxvalue=$(sed 's/,/\n/g' $csv | sed 's/\.[0-9]*//g' | uniq | sort -n | tail -n 1)
 meanvalue=$(sed 's/,/\n/g' $csv | uniq | sort -n | awk '{a+=$1} END{print a/NR}' | sed 's/\.[0-9]*//' )
 echo meanvalue is $meanvalue
 
 echo P2 > $pgm
-echo "116 116" >> $pgm
+echo "$n $n" >> $pgm
 echo $meanvalue >> $pgm
-cat $csv >> $pgm
+sed 's/\.[0-9]*//g' $csv >> $pgm
 sed -i  's/,/ /g' $pgm
 convert $pgm -resize 800x800. $png
