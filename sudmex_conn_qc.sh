2a4fbd1d138deb4caf7a432a9c6ce40c4f3dd1f0
Luis Concha <lconcha@gmail.com>
2024-05-27 09:40:47 -0600

    added matrix png

--- sudmex_conn_qc.sh
+++ sudmex_conn_qc.sh
@@ -6,7 +6,7 @@ source $(dirname $0)/sudmex_conn_env.sh
 if [ "$#" -lt 2 ]
 then
   Error "Insufficient arguments.
-        `basename $0` <sID> <atlasVersion>"
+     `basename $0` <sID> <atlasVersion>"
   exit 2
 fi
 
@@ -18,13 +18,16 @@ labels_version=$2
 fa=${out_dir}/${sID}/fa.mif
 labels_std=${sudmex_dir}/atlases/${labels_version}
 labels=${out_dir}/${sID}/dwispace_$(basename $labels_std)
+labels16=${out_dir}/${sID}/dwispace_$(basename ${labels_std%.nii.gz})_16.mif
 lab=${labels_version%.nii.gz}
 tck_sifted=${out_dir}/${sID}/sifted.tck
-connectome=${out_dir}/${sID}/connectome_sift2_${lab}.csv
+connectome_sift2=${out_dir}/${sID}/connectome_sift2_${lab}.csv
+connectome=${out_dir}/${sID}/connectome_${lab}.csv
+
 
 tmpDir=$(mktemp -d)
 
-mrconvert -datatype uint32 $labels ${tmpDir}/labels.mif
+mrconvert -datatype uint32 $labels $labels16
 
 my_do_cmd mrview \
   $fa \
@@ -41,11 +44,11 @@ my_do_cmd mrview \
   -tractography.load $tck_sifted \
   -overlay.opacity 0 \
   -capture.grab \
-  -connectome.init ${tmpDir}/labels.mif \
-  -connectome.load $connectome \
   -tractography.opacity 0 \
-  -reset \
+  -connectome.init $labels16 \
+  -connectome.load $connectome_sift2 \
   -size 800,600 \
+  -reset \
   -capture.grab \
   -exit 
   
@@ -53,3 +56,9 @@ this_gif=${out_dir}/${sID}/qc.gif
 convert -delay 30 -morph 5 ${tmpDir}/*.png $this_gif
 
 rm -fR $tmpDir
+
+
+for csv in $connectome $connectome_sift2
+do
+    sudmex_conn_csv2png.sh $csv ${csv%.csv}.png
+done
