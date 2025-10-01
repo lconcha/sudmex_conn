2218f8845ca18fae521cf7e0df3c2251e23b5d82
Luis Concha <lconcha@gmail.com>
2025-10-01 13:48:59 -0600

    handle sessions, and exit if previous results found

--- sudmex_conn_act.sh
+++ sudmex_conn_act.sh
@@ -3,10 +3,26 @@ source $(dirname $0)/sudmex_conn_env.sh
 
 
 sID=$1
+session=$2
 
-dwis=${out_dir}/${sID}/dwis_preproc.mif
-mask=${out_dir}/${sID}/mask.mif
-fod=${out_dir}/${sID}/wm_fod.mif
+
+fod=${out_dir}/${sID}/${session}/wm_fod.mif
+act_tck=${out_dir}/${sID}/${session}/nobackup/act.tck
+sift_tck=${out_dir}/${sID}/${session}/sifted.tck
+sift2_weights=${out_dir}/${sID}/${session}/sift2_weights.txt
+
+if [ -f $sift2_weights ]
+then
+  Warning "SIFT2 weights exists: $sift2_weights"
+  exit 0
+fi
+
+
+if [ ! -f $fod ]
+then
+  Error "FOD does not exist: $fod"
+  exit 2
+fi
 
 
 if [ ! -d ${out_dir}/${sID} ]
@@ -15,22 +31,23 @@ then
 fi
 
 
-aseg=${sudmex_dir}/Freesurfer/${sID}_T1w/mri/aparc+aseg.mgz
-fivett=${out_dir}/${sID}/5tt.mif
+aseg=${SUBJECTS_DIR}/${sID}_${session}/mri/aparc+aseg.mgz
+fivett=${out_dir}/${sID}/${session}/5tt.mif
 my_do_cmd 5ttgen freesurfer \
  -sgm_amyg_hipp \
   $aseg \
   $fivett
 
-gmwmi=${out_dir}/${sID}/gmwmi.mif
+gmwmi=${out_dir}/${sID}/${session}/gmwmi.mif
 my_do_cmd 5tt2gmwmi $fivett $gmwmi
 
-if [ ! -d ${out_dir}/${sID}/nobackup/ ]
+if [ ! -d ${out_dir}/${sID}/${session}/nobackup/ ]
 then 
-  mkdir -p ${out_dir}/${sID}/nobackup/
+  mkdir -p ${out_dir}/${sID}/${session}/nobackup/
 fi
 
-act_tck=${out_dir}/${sID}/nobackup/act.tck
+
+
 my_do_cmd tckgen \
   -act $fivett \
   -seed_gmwmi $gmwmi \
@@ -38,7 +55,6 @@ my_do_cmd tckgen \
   $fod \
   $act_tck
 
-sift_tck=${out_dir}/${sID}/sifted.tck
 my_do_cmd tcksift \
   -act $fivett \
   -term_number $sift_ntracks \
@@ -47,13 +63,10 @@ my_do_cmd tcksift \
   $fod \
   $sift_tck
 
-
-
-sift2_weights=${out_dir}/${sID}/sift2_weights.txt
 my_do_cmd tcksift2 \
   -act $fivett \
   -fd_scale_gm \
-  -out_mu ${out_dir}/${sID}/sift2_mu.txt \
+  -out_mu ${out_dir}/${sID}/${session}/sift2_mu.txt \
   $act_tck \
   $fod \
   $sift2_weights
